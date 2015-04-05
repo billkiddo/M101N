20287b8ddbca6501818d605f8850e5041ca55ffa
abilauca <adrian_bilauca@yahoo.com>
2015-04-05 19:50:05 +0300

    Completed H3.2, Week3

---
+++ H3.2/M101DotNet.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using M101DotNet.WebApp.Models;
+using M101DotNet.WebApp.Models.Home;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace M101DotNet.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var blogContext = new BlogContext();
+            // XXX WORK HERE
+            // find the most recent 10 posts and order them
+            // from newest to oldest
+            var recentPosts =
+                await
+                    blogContext.Posts.Find(new BsonDocument())
+                        .Sort(Builders<Post>.Sort.Descending(p => p.CreatedAtUtc))
+                        .Limit(10)
+                        .ToListAsync();
+
+            var model = new IndexModel
+            {
+                RecentPosts = recentPosts
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NewPost()
+        {
+            return View(new NewPostModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewPost(NewPostModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var blogContext = new BlogContext();
+            // XXX WORK HERE
+            // Insert the post into the posts collection
+            var user = Request.GetOwinContext().Authentication.User;
+            var post = new Post
+            {
+                Author = user.Identity.Name,
+                CreatedAtUtc = DateTime.Now.ToUniversalTime(),
+                Title = model.Title,
+                Content = model.Content
+            };
+
+            foreach (var tag in model.Tags.Split(','))
+                post.Tags.Add(tag);
+
+            await blogContext.Posts.InsertOneAsync(post);
+
+            return RedirectToAction("Post", new {id = post.Id});
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Post(string id)
+        {
+            var blogContext = new BlogContext();
+
+            // XXX WORK HERE
+            // Find the post with the given identifier
+            var post = await blogContext.Posts.Find(p => p.Id.Equals(id)).SingleOrDefaultAsync();
+
+            if (post == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var model = new PostModel
+            {
+                Post = post
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Posts(string tag = null)
+        {
+            var blogContext = new BlogContext();
+
+            // XXX WORK HERE
+            // Find all the posts with the given tag if it exists.
+            // Otherwise, return all the posts.
+            // Each of these results should be in descending order.
+            var posts =
+                await
+                    blogContext.Posts.Find(new BsonDocument())
+                        .Sort(Builders<Post>.Sort.Descending(p => p.CreatedAtUtc))
+                        .ToListAsync();
+
+            return View(posts);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewComment(NewCommentModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Post", new {id = model.PostId});
+            }
+
+            var blogContext = new BlogContext();
+            // XXX WORK HERE
+            // add a comment to the post identified by model.PostId.
+            // you can get the author from "this.User.Identity.Name"
+            var user = Request.GetOwinContext().Authentication.User;
+            var comment = new Comment
+            {
+                Author = user.Identity.Name,
+                CreatedAtUtc = DateTime.Now.ToUniversalTime(),
+                Content = model.Content
+            };
+
+            await
+                blogContext.Posts.FindOneAndUpdateAsync(
+                    p => p.Id.Equals(model.PostId),
+                    Builders<Post>.Update.Push(p => p.Comments, comment));
+
+            return RedirectToAction("Post", new {id = model.PostId});
+        }
+    }
+}
---
+++ H3.2/M101DotNet.WebApp/Models/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace M101DotNet.WebApp.Models
+{
+    public class Post
+    {
+        // XXX WORK HERE
+        // add in the appropriate properties for a post
+        // The homework instructions contain the schema.
+        public Post()
+        {
+            Tags = new List<string>();
+            Comments = new List<Comment>();
+        }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public IList<string> Tags { get; private set; }
+
+        public DateTime CreatedAtUtc { get; set; }
+
+        public IList<Comment> Comments { get; private set; }
+    }
+}
